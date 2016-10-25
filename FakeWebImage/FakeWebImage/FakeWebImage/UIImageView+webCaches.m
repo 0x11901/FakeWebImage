fcdd89c3a33b463ed3046bf186d2cb692f0d2095
王靖凯 <wjk930726@live.com>
2016-10-25 22:32:53 +0800

    changing cancel operation way

--- FakeWebImage/FakeWebImage/FakeWebImage/UIImageView+webCaches.m
+++ FakeWebImage/FakeWebImage/FakeWebImage/UIImageView+webCaches.m
@@ -22,4 +22,26 @@
     }];
 }
 
+/*
+
+- (void)fake_setImageWithURLString:(NSString *)URLString{
+    if ([URLString isEqualToString:self.lastURlString]) {
+        [[DownloaderOperationManger sharedManger] cancelDownloadOperation];
+    }
+    self.lastURlString = URLString;
+    [[DownloaderOperationManger sharedManger] manger_donwloadImageWithURL:URLString successBlock:^(UIImage *image){
+        self.image = image;
+    }];
+}
+
+- (void)setLastURlString:(NSString *)lastURlString{
+    objc_setAssociatedObject(self, "key", lastURlString, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (NSString *)lastURlString{
+    return objc_getAssociatedObject(self, "key");
+}
+
+ */
+
 @end
